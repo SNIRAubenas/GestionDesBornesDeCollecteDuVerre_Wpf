449eaa4b79b9c5a39c9b0115c9b23f9de7b9bd2c
dpysarenko <dpysarenko@BTS-4F78>
2026-02-04 16:30:58 +0100

    Mise en place du serveur d’applications et connexion à la base de données MySQL
    - Développement d’un serveur d’applications en C#
    - Lecture et exploitation des données depuis la base
    - Structuration du code (DatabaseManager, MesureService)

---
+++ ServeurBornesCollecte/DatabaseManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ServeurBornesCollecte
+{
+    public class DatabaseManager
+    {
+        private string connectionString =
+            "Server=localhost;" +
+            "Database=gestion_bornes;" +
+            "User=root;" +
+            "Password=;";
+
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ ServeurBornesCollecte/MesureService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace ServeurBornesCollecte
+{
+    public class MesureService
+    {
+        private DatabaseManager dbManager = new DatabaseManager();
+
+        public void AfficherMesures()
+        {
+            using (MySqlConnection connection = dbManager.GetConnection())
+            {
+                string sql =
+                    "SELECT niveau_remplissage, date_mesure " +
+                    "FROM mesure " +
+                    "ORDER BY date_mesure DESC";
+
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int niveau = reader.GetInt32("niveau_remplissage");
+                    DateTime date = reader.GetDateTime("date_mesure");
+
+                    Console.WriteLine($"Niveau : {niveau}% | Date : {date}");
+                }
+
+                reader.Close();
+            }
+        }
+    }
+}
