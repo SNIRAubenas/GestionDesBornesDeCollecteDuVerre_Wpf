46d2626afc4396529f799068594abe58cd7f6764
dpysarenko <dpysarenko@BTS-4F78>
2026-03-02 10:15:41 +0100

    Implémentation du client MQTT avec réception des messages depuis Mosquitto. Connexion au broker local, abonnement au topic capteur/niveau et affichage des messages reçus en console.

--- ServeurBornesCollecte/Program.cs
+++ ServeurBornesCollecte/Program.cs
@@ -1,15 +1,41 @@
-﻿using ServeurBornesCollecte.Core;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
 
-namespace ServeurBornesCollecte
+class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            MesureService service = new MesureService();
-            service.AfficherMesures();
+        var factory = new MqttClientFactory();
+        var client = factory.CreateMqttClient();
 
-            Console.ReadKey();
-        }
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 1883)
+            .Build();
+
+        client.ApplicationMessageReceivedAsync += e =>
+        {
+            var payload = e.ApplicationMessage.Payload;
+
+            if (!payload.IsEmpty)
+            {
+                var message = Encoding.UTF8.GetString(payload);
+                Console.WriteLine("Message reçu : " + message);
+            }
+
+            return Task.CompletedTask;
+        };
+
+        await client.ConnectAsync(options);
+        Console.WriteLine("Connecté au broker MQTT");
+
+        await client.SubscribeAsync("capteur/niveau");
+        Console.WriteLine("Abonné au topic capteur/niveau");
+
+        Console.WriteLine("Appuyez sur une touche pour quitter...");
+        Console.ReadKey();
+
+        await client.DisconnectAsync();
     }
 }
